6eb4b50fa3cf59837a4683cb5602c8c71163a134
sdickey <sean.dickey@live.com>
2014-08-23 10:09:42 -0700

    Added empty rails skeleton to project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SwsRailsApp::Application.config.secret_key_base = 'd10bed7b59aaf6b67d3b0d058e1d8358ba654c9ba023f0990cfa6281f75e86205e219dd863dbee218c6e959f83ee2a970e983da1a54d8c6daf0a6a2bb9377d62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SwsRailsApp::Application.config.session_store :cookie_store, key: '_sws-rails-app_session'
