d517ebca4d4933b3e14bb6183fb3c18b940951bd
sdickey <sean.dickey@live.com>
2014-08-23 15:54:17 -0700

    Removed username parameter from account_params and added email parameter.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -14,6 +14,6 @@ class AccountsController < ApplicationController
 
   private
     def account_params
-      params.require(:account).permit(:username, :password, :password_confirmation)
+      params.require(:account).permit(:email, :password, :password_confirmation)
     end
 end
