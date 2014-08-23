95bfa3be8d4c5488cc3d87d0c1964df0fa8cda68
sdickey <sean.dickey@live.com>
2014-08-23 12:50:21 -0700

    Added #create action and strong params for creating accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,4 +2,16 @@ class AccountsController < ApplicationController
   def new
 
   end
+
+  def create
+    @account = Account.new(account_params)
+    if @account.save
+      redirect_to new_owner_path
+    end
+  end
+
+  private
+    def account_params
+      params.require(:account).permit(:username, :password, :password_confirmation)
+    end
 end
