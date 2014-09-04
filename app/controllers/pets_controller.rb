7fec63e52b3e507552d10ce61784ce0134084c49
sdickey <sean.dickey@live.com>
2014-09-04 15:37:27 -0700

    Added actions to create a pet on an account

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,19 @@
 class PetsController < ApplicationController
+  def new
+    @account = Account.find(params[:account_id])
+  end
+
+  def create
+    @account = Account.find(params[:account_id])
+    @pet = @account.pets.create(pet_params)
+    render 'new'
+  end
+
+  private
+    def pet_params
+      params.require(:pet).permit(:name, :kind, :breed, :age, :birth_month,
+                                  :health_history, :current_health, :med_name,
+                                  :med_administer, :med_dose, :med_frequency,
+                                  :behavior)
+    end
 end
