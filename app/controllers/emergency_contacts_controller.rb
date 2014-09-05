0dd6cd8a45991a06695fd5d41a27128a8354a80c
sdickey <sean.dickey@live.com>
2014-09-04 23:11:24 -0700

    Added new and create actions  to emergency contacts controller

--- app/controllers/emergency_contacts_controller.rb
+++ app/controllers/emergency_contacts_controller.rb
@@ -1,2 +1,20 @@
 class EmergencyContactsController < ApplicationController
+  def index
+  end
+
+  def new
+    @account = Account.find(params[:account_id])
+  end
+
+  def create
+    @account = Account.find(params[:account_id])
+    @emergency_contact = @account.emergency_contacts.build(emergency_contact_params)
+  end
+
+  private
+    def emergency_contact_params
+      params.require(:emergency_contact).permit(:emerg_first_name, :emerg_last_name,
+                     :emerg_hphone, :emerg_wphone, :emerg_cphone)
+      render 'new'
+    end
 end
