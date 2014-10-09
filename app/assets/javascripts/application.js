d617ab0afb1e4f1d10be4417e9f36c8377af9d11
sdickey <sean.dickey@live.com>
2014-10-08 20:52:25 -0700

    Added gcal.js file for fullcalendar functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,5 @@
 //= require turbolinks
 //= require jquery-readyselector
 //= require fullcalendar
+//= require gcal
 //= require_tree .
--- app/assets/javascripts/reservations.js
+++ app/assets/javascripts/reservations.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
   $('#calendar').fullCalendar({
-
-    events: 'https://www.google.com/calendar/feeds/4dahvsn39nbms77s9i5njidt9g%40group.calendar.google.com/public/basic'
-    // eventSources: [
-    //   {
-    //     url: 'https://www.google.com/calendar/feeds/4dahvsn39nbms77s9i5njidt9g%40group.calendar.google.com/public/basic',
-    //     dataType: 'jsonp'
-    //   }
-    // ]
+    eventSources: [
+      {
+        url: 'https://www.google.com/calendar/feeds/4dahvsn39nbms77s9i5njidt9g%40group.calendar.google.com/public/basic',
+        dataType: 'jsonp'
+      }
+    ]
   });
 });
 
