9d42f4a0e55cdea29e31b1498a4fcfc013b792bf
sdickey <sean.dickey@live.com>
2014-10-09 12:12:37 -0700

    Added 'gcal' data type to reservations.js for proper sync with google calendar

--- app/assets/javascripts/reservations.js
+++ app/assets/javascripts/reservations.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     eventSources: [
       {
         url: 'https://www.google.com/calendar/feeds/4dahvsn39nbms77s9i5njidt9g%40group.calendar.google.com/public/basic',
-        dataType: 'jsonp'
+        dataType: 'gcal'
       }
     ]
   });
