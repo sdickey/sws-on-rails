8050c5d931aaa34d9c7fe23ac344ef7bc0d848bb
sdickey <sean.dickey@live.com>
2014-12-20 13:35:29 -0800

    Updated production email delivery/sending addresses to Marina's SWS account

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -3,18 +3,18 @@ class ReservationMailer < ActionMailer::Base
   def reservation_requested(reservation, account)
     @reservation = reservation
     @account = account
-    mail to: 'sean.a.dickey5@gmail.com', from: account.email, subject: 'SWS Reservation Request'
+    mail to: 'marina@shakewagglestroll.com', from: account.email, subject: 'SWS Reservation Request'
   end
 
   def reservation_update_by_admin(reservation, account)
     @reservation = reservation
     @account = account
-    mail to: account.email, from: 'sean.a.dickey5@gmail.com', subject: 'Pet Sitting Reservation Update'
+    mail to: account.email, from: 'marina@shakewagglestroll.com', subject: 'Pet Sitting Reservation Update'
   end
 
   def reservation_update_by_client(reservation, account)
     @reservation = reservation
     @account = account
-    mail to: 'sean.a.dickey5@gmail.com', from: account.email, subject: 'Client Update to Reservation'
+    mail to: 'marina@shakewagglestroll.com', from: account.email, subject: 'Client Update to Reservation'
   end
 end
