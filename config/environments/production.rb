45a1d39a648f28b4d0bb6bc3c72e2579ba402276
sdickey <sean.dickey@live.com>
2014-12-20 13:24:43 -0800

    Added Action Mailer settings for production environment on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,26 +69,26 @@ SwsRailsApp::Application.configure do
   # http://rubyonrailshelp.wordpress.com/2014/01/02/setting-up-mailer-using-devise-for-forgot-password/
 
   #########//TURN ON IN PRODUCTION//#########################
-  # config.action_mailer.default_url_options = {
-  #   :host => 'yoursite.herokuapp.com'
-  # }
+  config.action_mailer.default_url_options = {
+    :host => ENV['MAILER_HOST']
+  }
 
-  # Rails.application.routes.default_url_options[:host] = 'shakewagglestroll.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = ENV['MAILER_HOST']
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = false
-  # config.action_mailer.default :charset => 'utf-8'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => 'utf-8'
 
-  # config.action_mailer.smtp_settings = {
-  #   address: "smtp.gmail.com",
-  #   port: 587,
-  #   domain: ENV["GMAIL_DOMAIN"],
-  #   authentication: "plain",
-  #   enable_starttls_auto: true,
-  #   user_name: ENV["GMAIL_USERNAME"],
-  #   password: ENV["GMAIL_PASSWORD"]
-  # }
+  config.action_mailer.smtp_settings = {
+    address: "host21.cyberbasement.com",
+    port: 26, #gives auth error when on port 26
+    domain: "www.shakewagglestroll.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["SWSMAIL_USERNAME"],
+    password: ENV["SWSMAIL_PASSWORD"]
+  }
   ##########################################################
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
